9219a00e86c19964db2f1480fed2a3371d7a65b0
SahTitus <sahtitus58@gmail.com>
2023-09-21 23:40:43 +0000

    first commit

---
+++ .history/tailwind.config_20230921233544.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],git branch -M main
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
